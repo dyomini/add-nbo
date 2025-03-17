83e7e4173aea933c5a0d06e3cc407b38db39d2c9
m1n <dongminyu9@gmail.com>
2025-03-17 22:25:32 +0900

    2025ver.

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
-add-nbo: main.c
-gcc -o add-nbo main.c
+all: sum-nbo
+
+sum-nbo: main.o
+	gcc -o sum-nbo main.o
+
+main.o: main.c
+	gcc -c -o main.o main.c
+
+clean:
+	rm -f main main.o
--- main.c
+++ main.c
@@ -2,40 +2,46 @@
 #include <stdint.h>
 #include <netinet/in.h>
 
-int main(int argc, char* argv[]){
-	if (argc!=3){
-		printf("syntax : add-nbo <file1> <file2>\n");
-		printf("sample : add-nbo a.bin b.bin\n");
-		printf("please select two files.\n");
-		return 1;
-	}
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        printf("syntax : sum-nbo <file1> [<file2>...]\n");
+        printf("sample : sum-nbo a.bin b.bin\n");
+        printf("please select at least two files.\n");
+        return 1;
+    }
 
-	uint32_t x,y,res1,res2;
-	FILE *fp1,*fp2;
+    uint32_t x, sum = 0, res;
+    FILE *fp;
 
-	fp1=fopen(argv[1], "rb");
-	fp2=fopen(argv[2],"rb");
+    for (int i = 1; i < argc; i++) {
+        fp = fopen(argv[i], "rb");
 
-	if(res1!=1||res2!=1){
-		printf("Files cannot be read.\n");
-		return 1;
-	}
+        if (fp == NULL) {
+            printf("File %s cannot be opened.\n", argv[i]);
+            return 1;
+        }
 
-	res1=fread(&x,sizeof(uint32_t),1,fp1);
-	res2=fread(&y,sizeof(uint32_t),1,fp2);
+        res = fread(&x, sizeof(uint32_t), 1, fp);
 
-	if(fp1==NULL||fp2==NULL){
-		printf("Files cannot be opened.\n");
-		return 1;
-	}
-	x=ntohl(x);
-	y=ntohl(y);
+        if (res != 1) {
+            printf("File %s is too small to read 4 bytes.\n", argv[i]);
+            fclose(fp);
+            return 1;
+        }
 
-	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", x, x, y, y, x+y, x+y);
+        x = ntohl(x);
+        printf("%d(0x%x)", x, x);
 
-	fclose(fp1);
-	fclose(fp2);
+        sum += x;
 
-	return 0;
+        fclose(fp);
+
+        if (i < argc - 1) {
+            printf(" + ");
+        }
+    }
+
+    printf(" = %d(0x%x)\n", sum, sum);
+
+    return 0;
 }
-//파일이 제대로 열리지 않는 경우, 파일의 크기가 4바이트보다 작아서 제대로 읽지 못했다고 판단되는 경우 
